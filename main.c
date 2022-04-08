42be19c4fe1bb576b5c1cc896d5b168c603548da
mguru2019 <32654507+mguru2019@users.noreply.github.com>
2022-04-08 00:44:00 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 #include <stdio.h>
 
 char String[25];
-float duty_cycle = 0.75;
+double duty_cycle = 50;
 int flag = 0;
 int rising = 0;
 int falling = 0;
@@ -32,14 +32,14 @@ tire* front_right;
 
 void Initialize(void) {
 	
-
+	cli();
 	
 	//BLUE: BACK LEFT
 	
-	// speed
+	// backwards
 	DDRD |= (1<<DDD2);
 	
-	//backwards
+	//speed
 	DDRD |= (1<<DDD3);
 	
 	//forwards
@@ -58,10 +58,10 @@ void Initialize(void) {
 	
 	//WHITE: FRONT RIGHT
 	
-	//speed
+	//backwards
 	DDRB |= (1<<DDB0);
 	
-	//backwards
+	//speed
 	DDRB |= (1<<DDB1);
 	
 	//forwards
@@ -129,10 +129,87 @@ void Initialize(void) {
 	//Start conversion
 	ADCSRA |= (1<<ADSC);
 	
+	//TIMER 0 in fast PWM mode
+	TCCR0A |= (1<<WGM00);
+	TCCR0A |= (1<<WGM01);
+	TCCR0B &= ~(1<<WGM02);
+	
+	//TIMER 1 in fast PWM mode
+	TCCR1A |= (1<<WGM10);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1B |= (1<<WGM12);
+	TCCR1B &= ~(1<<WGM13);
+	
+	//TIMER 2 in fast PWM mode
+	TCCR2A |= (1<<WGM20);
+	TCCR2A |= (1<<WGM21);
+	TCCR2B &= ~(1<<WGM22);
+
+	//TIMER 0 in fast PWM mode
+	TCCR0A |= (1<<WGM00);
+	TCCR0A |= (1<<WGM01);
+	TCCR0B &= ~(1<<WGM02);
+	
+	// TIMER 0 prescale by 1024
+	TCCR0B |= (1<<CS00);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B |= (1<<CS02);
+	
+	// TIMER 1 prescale by 1024
+	TCCR1B |= (1<<CS10);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B |= (1<<CS12);
+	
+	// TIMER 2 prescale by 1024
+	TCCR2B |= (1<<CS10);
+	TCCR2B |= (1<<CS11);
+	TCCR2B |= (1<<CS12);
+	
+	//TIMER 0 Output Compare
+	TCCR0A |= (1<<COM0B1);
+	TCCR0A &= ~(1<<COM0B0);
+	
+	OCR0B = (duty_cycle/100) * 255;
+	
+	TIMSK0 |= (1<<TOIE0);
+	
+	//TIMER 1 Output Compare
+	TCCR1A |= (1<<COM1A1);
+	TCCR1A &= ~(1<<COM1A0);
+	
+	OCR1A = (duty_cycle/100) * 255;
+	
+	TIMSK1 |= (1<<TOIE1);
+	
+	//TIMER 2 Output Compare
+	TCCR2A |= (1<<COM2A1);
+	TCCR2A &= ~(1<<COM2A0);
+	TCCR2A |= (1<<COM2B1);
+	TCCR2A &= ~(1<<COM2B0);
+	
+	OCR2B = (duty_cycle/100) * 255;
+	OCR2A = (duty_cycle/100) * 255;
+	
+	TIMSK2 |= (1<<TOIE2);
+	
+	
+	sei();
+	
 }
 
+ISR(TIMER0_OVF_vect) {
+	OCR0B = (duty_cycle/100) * 255;
+}
 
+ISR(TIMER1_OVF_vect) {
+	OCR1A = (duty_cycle/100) * 255;
+}
 
+ISR(TIMER2_OVF_vect) {
+	OCR2B = (duty_cycle/100) * 255;
+	OCR2A = (duty_cycle/100) * 255;
+
+}
 
 int main(void)
 {
@@ -143,7 +220,7 @@ int main(void)
 	
 	
 	back_left = malloc(sizeof(*back_left));
-	init_tire(back_left, 0, PORTD2, PORTD4, PORTD3);
+	init_tire(back_left, 0, PORTD3, PORTD4, PORTD2);
 	
 	
 	front_left = malloc(sizeof(*front_left));
@@ -152,14 +229,16 @@ int main(void)
 	
 	
 	front_right = malloc(sizeof(*front_right));
-	init_tire(front_right, 1, PORTB0, PORTB2, PORTB1);
+	init_tire(front_right, 1, PORTB1, PORTB2, PORTB0);
 
 	back_right = malloc(sizeof(*back_right));
 	init_tire(back_right, 1, PORTB3, PORTB5, PORTB4);
 	
 	int item = 0;
+	//PORTD |= (1<<PORTD7);
 	
 	while(1) {
+		
 		
 		if (PINC & (1<<PINC5) && PINC & (1<<PINC3) && PINC & (1<<PINC2)) {
 			slide_right(front_right, back_right, front_left, back_left);
@@ -181,12 +260,32 @@ int main(void)
 			stop(front_right, back_right, front_left, back_left);
 		}
 		
+		
 		if (ADC > 900) {
+			duty_cycle = 100;
+			/*
 			item = choose_item();
 			sprintf(String, "item was %d \n", item);
 			UART_putstring(String);
 			deploy_item(item);
+			*/
+		} 
+		
+		sprintf(String, "ADC was %d\n", ADC);
+		UART_putstring(String);
+		
+		
+		/*
+		move_car_forwards(front_right, back_right, front_left, back_left);
+		
+		
+		_delay_ms(100);
+		duty_cycle += 10;
+		if (duty_cycle > 100) {
+			duty_cycle = 0;
 		}
+		
+		*/
 	
 		
 	}
