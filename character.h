10cfb542cd257339bb86a747d59ade74acbf3aef
veevinn <41170192+veevinn@users.noreply.github.com>
2022-04-13 11:39:37 -0400

    Create character.h

---
+++ character.h
@@ -0,0 +1,24 @@
+/*
+ * character.h
+ *
+ * Created: 4/13/2022 10:56:28 AM
+ *  Author: veevinn
+ */ 
+
+
+#ifndef CHARACTER_H_
+#define CHARACTER_H_
+
+#define MARIO 1
+#define LUIGI 2
+#define WALUIGI 3
+#define WARIO 4
+#define PEACH 5
+#define BOWSER 6
+#define YOSHI 7
+#define DAISY 8
+#define TOAD 9
+#define DONKEYKONG 10
+
+void choose_character();
+void light_led(int character);
