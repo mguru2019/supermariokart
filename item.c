9a2d1c727e618583492b9117f431dfb2e998f627
veevinn <41170192+veevinn@users.noreply.github.com>
2022-04-14 18:23:10 -0400

    Update item.c

--- item.c
+++ item.c
@@ -15,12 +15,10 @@ tire* back_right;
 tire* front_left;
 tire* front_right;
 
-int has_item = 0;
-
 int choose_item(void) {
 	
 	if (!has_item) {
-		int item = rand() % 9 + 1;
+		int item = rand() % 3 + 1;
 		has_item = 1;
 		return item;
 	}
@@ -28,36 +26,28 @@ int choose_item(void) {
 
 }
 
-void deploy_item(int item) {
-	
-	back_left = malloc(sizeof(*back_left));
-	init_tire(back_left, 0, PE5, PL7, PL6, 0);
-	
-	
-	front_left = malloc(sizeof(*front_left));
-	init_tire(front_left, 0, PE3, PL5, PL4, 0);
-	
-	
-	front_right = malloc(sizeof(*front_right));
-	init_tire(front_right, 1, PB4, PL1, PL0, 0);
+void deploy_item(int item, tire* fr, tire* br, tire* fl, tire* bl) {
+	if (has_item == 1) {
+		if (item == MUSHROOM) {
+			speed_up(fr,br,fl,bl);
+			_delay_ms(2000);
+			slow_down(fr,br,fl,bl);
+		}
+		else if (item == BANANA) {
+			_delay_ms(1000);
+			spin(fr,br,fl,bl);
+		}
+		else if (item == STAR) {
+			speed_up(fr,br,fl,bl);
+			for(int i = 0; i < 10; i++) {
+				random_pwm();
+				_delay_ms(500);
+			}
+			slow_down(fr,br,fl,bl);
+			light_led(user_character);
+		}
+		has_item = 0;
+	}
 
-	back_right = malloc(sizeof(*back_right));
-	init_tire(back_right, 1, PB5, PL3, PL2, 0);
-	
-	if (item == MUSHROOM) {
-		speed_up(front_right,back_right,front_left,back_left);
-		_delay_ms(1000);
-		slow_down(front_right,back_right,front_left,back_left);
-	}
-	else if (item == BANANA) {
-		spin(front_right,back_right,front_left,back_left);
-	}
-/*	else if (item == STAR) {
-		speed_up(front_right,back_right,front_left,back_left);
-		random_pwm();
-		_delay_ms(5000);
-		slow_down(front_right,back_right,front_left,back_left);
-		light_led(character); 
-	} */
-	has_item = 0;
 }
+
